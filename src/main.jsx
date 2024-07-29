3ab2f76a4177546828fbab62d8bec344344739b7
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-07-29 13:56:27 +0530

    configured tailwind

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// https://www.subframe.com/blog/how-to-make-tailwind-desktop-first
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
