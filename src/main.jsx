01593c8248ec7e601e29532caa5003c9d31e5152
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-07-30 13:56:06 +0530

    integrated and configured redux-toolkit and enabled react-dev-tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
+import store from './Redux/store.js';
 import App from './App.jsx'
+
+//css imports
 import './index.css'
+
 // https://www.subframe.com/blog/how-to-make-tailwind-desktop-first
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+
 )
