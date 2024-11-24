070f7cf376f480874b5657e071acd86d39697d32
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-11-24 22:16:47 +0530

    added line chart for visual interpretation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,28 @@ import HomeLayout from "../layouts/HomeLayout";
 import { TbProgressBolt } from "react-icons/tb";
 import { MdCancel, MdOutlineDoneAll, MdPending } from "react-icons/md";
 import { useTickets } from "../hooks/useTickets";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
-import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
+import { Pie,Line } from 'react-chartjs-2';
+import { useEffect, useState } from "react";
 
 
 
 
-ChartJS.register(ArcElement, Tooltip, Legend, Title);
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    Title,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement
+);
 
 export default function Home() {
 
-    const [ticketState] = useTickets()
+    const [ticketState] = useTickets();
+    const [openTicketsData, setOpenTicketsData] = useState({});
     const pieChartData = {
         labels: Object.keys(ticketState.ticketDistribution),
         datasets: [
@@ -34,6 +45,34 @@ export default function Home() {
             }
         ]
     }
+
+    useEffect(()=>{
+        if(ticketState.downloadedTicketList.length){
+            let openTicketsData = {
+
+            }
+            ticketState.downloadedTicketList.forEach((ticket)=>{
+                if(ticket.status == 'open'){
+                    const date = ticket.createdAt.split("T")[0];
+                    openTicketsData[date]= !openTicketsData.hasOwnProperty(date) ? 1 : openTicketsData[date]+1
+                }
+            })
+            console.log(openTicketsData);
+            setOpenTicketsData(openTicketsData)
+        }
+    },[ticketState.downloadedTicketList])
+
+    const lineChartData = {
+        labels : Object.keys(openTicketsData),
+        datasets: [
+          {
+            label: 'Open Tickets Data',
+            data: Object.values(openTicketsData),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          }      
+        ]
+      };
 
     return (
         <HomeLayout>
@@ -97,8 +136,12 @@ export default function Home() {
                 <div>
                     <Pie
                         data={pieChartData}
-                        backgroundColor
                     />
+                </div>
+            </div>
+            <div>
+            <div className="mt-10 flex justify-center items-center">
+                <Line data={lineChartData} />
                 </div>
             </div>
         </HomeLayout>
